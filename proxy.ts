de760bd6b6a25aa343d29a3d6deb4dd58916b925
Vivek Darak <vivekdarak@gmail.com>
2026-08-03 11:48:33 +0530

    Redirect www domain to canonical host

--- proxy.ts
+++ proxy.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const OLD_DOMAIN = "dantam.dental-care.co.in";
+const WWW_DOMAIN = "www.dantamdentalcare.com";
+const CANONICAL_ORIGIN = "https://dantamdentalcare.com";
 const NEW_HOME_URL = "https://dantamdentalcare.com/";
 
 export function proxy(request: NextRequest) {
@@ -9,6 +11,11 @@ export function proxy(request: NextRequest) {
 
   if (host === OLD_DOMAIN) {
     return NextResponse.redirect(NEW_HOME_URL, 301);
+  }
+
+  if (host === WWW_DOMAIN) {
+    const canonicalUrl = new URL(request.nextUrl.pathname + request.nextUrl.search, CANONICAL_ORIGIN);
+    return NextResponse.redirect(canonicalUrl, 301);
   }
 
   return NextResponse.next();
