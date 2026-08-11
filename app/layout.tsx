b0cef172655c104336d4590144b2563dbafa7d19
Vivek Darak <vivekdarak@gmail.com>
2026-08-11 12:05:10 +0530

    Mark smooth scroll behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <head>
         {openinaryBaseUrl && (
           <>
